df5c5498a82df7b03c3ee612cbff2377c8a08786
Wootsz <wouterbrozius@hotmail.com>
2021-02-16 22:54:10 +0100

    waariswouter levels

--- wiw_script.js
+++ wiw_script.js
@@ -1,45 +1,53 @@
+// ------------------- Init functions -------------------
 
-var level_index = -1;
-
-// Note the order of elements in this array determines the order of the levels
-// Y -= 95
-var all_levels = [
-    ["mensen22", "586, 226,621, 228,634, 240,653, 223,678, 224,678, 246,650, 255,677, 285,655, 311   ,626, 312,597, 289,619, 260,582, 240"],
-    ["mensen25", " 505, 325, 621, 340, 592, 426, 509, 412"],
-    ["mensen2",  ""],
-    ["mensen19", ""],
-    ["mensen10", ""],
-    ["mensen14", ""],
-    ["mensen27", ""],
-    ["mensen24", ""],
-    ["mensen26", ""],
-    ["mensen15", ""],
-    ["mensen13", ""],
-    ["mensen9",  ""],
-    ["mensen1",  ""],
-    ["mensen3",  ""],
-    ["mensen16", ""],
-    ["mensen23", ""],
-    ["mensen12", ""],
-    ["mensen17", ""],
-    ["mensen18", ""],
-    ["mensen7",  ""],
-    ["mensen20", ""],
-    ["mensen28", ""],
-    ["mensen21", ""],
-    ["mensen8",  ""],
-    ["mensen6",  ""],
-    ["mensen5",  ""],
-    ["mensen4",  ""],
-    ["mensen11", ""]
-]
+window.addEventListener("load", function() {
+	document.getElementsByClassName("waldo")[0].addEventListener("click", checkClickLocation);
+})
 
 window.onload = function init() {
     complete_level();
 }
 
+// ------------------- Level Selection and Data -------------------
+
+var level_index = -1;
+var rectangle = "r";
+var triangle = "t";
+
+// Note the order of elements in this array determines the order of the levels
+var all_levels = [
+    ["mensen22", rectangle, [782,303,898,410]],
+    ["mensen25", rectangle, [678,434,775,568]],
+    ["mensen2",  rectangle, [913,331,1026,468]],
+    ["mensen19", rectangle, [312,191,434,357]],
+    ["mensen10", rectangle, [912,49,969,137]],
+    ["mensen14", rectangle, [803,763,990,976]],
+    ["mensen27", rectangle, [634,464,719,584]],
+    ["mensen24", rectangle, [834,775,908,847]],
+    ["mensen26", rectangle, [895,424,966,503]],
+    ["mensen15", rectangle, [953,195,1014,272]],
+    ["mensen13", rectangle, [144,466,208,543]],
+    ["mensen9",  rectangle, [648,489,1068,940]],
+    ["mensen1",  rectangle, [1337,325,1418,457]],
+    ["mensen3",  rectangle, [518,417,561,477]],
+    ["mensen16", rectangle, [1490,905,1543,980]],
+    ["mensen23", triangle,  [1795,122,1688,206,1798,255]],
+    ["mensen12", rectangle, [260,412,317,465]],
+    ["mensen17", triangle,  [1715,0,1795,4,1797,93]],
+    ["mensen18", rectangle, [816,269,934,427]],
+    ["mensen7",  rectangle, [898,287,966,344]],
+    ["mensen20", triangle,  [0,1218,92,1348,0,1348]],
+    ["mensen28", rectangle, [1247,205,1290,303]],
+    ["mensen21", rectangle, [1271,2,1333,67]],
+    ["mensen8",  rectangle, [14,94,84,199]],
+    ["mensen6",  rectangle, [1494,558,1542,626]],
+    ["mensen5",  rectangle, [1151,165,1182,195]],
+    ["mensen4",  rectangle, [841,77,854,91]],
+    ["mensen11", rectangle, [838,409,859,431]]
+]
+
 function complete_level() {
-    if (level_index == all_levels.length) {
+    if (level_index >= all_levels.length - 1) {
         document.location.href = "waariswouterfinallevel.html";
     } else {
         level_index += 1;
@@ -47,12 +55,84 @@ function complete_level() {
         console.log(current_level);
         document.getElementById("level_header_text").innerHTML  = "Level " + (level_index + 1);
         document.getElementById("picture").src = "resources/images/waariswouter/" + current_level[0] + ".jpg";
-        document.getElementById("wouter_area").coords = current_level[1];
     }
 }
 
-function display_coords() {
-    var x = window.event.clientX;
-    var y = window.event.clientY;
-    console.log("iks " + x + ", " + (y - 95))
+function checkClickLocation(event) {
+	var click_event = window.event;
+	var x = click_event.offsetX?(click_event.offsetX):click_event.
+		pageX - document.getElementsByClassName("waldo").offsetLeft;
+	var y = click_event.offsetY?(click_event.offsetY):click_event.
+		pageY - document.getElementsByClassName("waldo").offsetTop;
+    console.log(x + "," + y);
+    
+    var current_level = all_levels[level_index];
+    var current_shape = current_level[1];
+    var current_coordinates = current_level[2];
+
+    switch(current_shape) {
+        case rectangle: 
+            if (isInRectangle(x, y, current_coordinates)) {
+                complete_level();
+            }
+            break;
+        case triangle:
+            if (isInTriangle(x, y, current_coordinates)) {
+                complete_level();
+            }
+            break;
+        default: complete_level(); 
+    }
 }
+
+function isInRectangle(x, y, rect) {
+    return x >= rect[0] && x <= rect[2] && y >= rect[1] && y <= rect[3];
+}
+
+function isInTriangle(x, y, tri) {
+    x1 = tri[0];
+    y1 = tri[1];
+    x2 = tri[2];
+    y2 = tri[3];
+    x3 = tri[4];
+    y3 = tri[5];
+
+    /* Calculate area of triangle ABC */
+   var A = area (x1, y1, x2, y2, x3, y3); 
+  
+   /* Calculate area of triangle PBC */   
+   var A1 = area (x, y, x2, y2, x3, y3); 
+  
+   /* Calculate area of triangle PAC */   
+   var A2 = area (x1, y1, x, y, x3, y3); 
+  
+   /* Calculate area of triangle PAB */    
+   var A3 = area (x1, y1, x2, y2, x, y); 
+    
+   /* Check if sum of A1, A2 and A3 is same as A */ 
+   return (A == A1 + A2 + A3); 
+}
+
+function area(x1, y1, x2, y2, x3, y3) { 
+   return Math.abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0); 
+} 
+  
+
+// function display_coords(event) {
+//     var x = event.pageX - this.offsetLeft;
+//     var y = event.pageY - this.offsetTop;
+//     console.log(x + ", " + (y - 95))
+// }
+
+
+// ------------------- Image Zoom (TODO) -------------------
+
+// function zoom(e){
+//     var zoomer = e.currentTarget;
+//     e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+//     e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+//     x = offsetX/zoomer.offsetWidth*100
+//     y = offsetY/zoomer.offsetHeight*100
+//     zoomer.style.backgroundPosition = x + '% ' + y + '%';
+// }
+
