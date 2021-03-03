b611c65eadb43a963111493bd0480e6646f4d0ac
Wootsz <wouterbrozius@hotmail.com>
2021-03-03 20:07:16 +0100

    multiple languages

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,32 @@ function addP(name) {
     text = document.createElement()
     element.appendChild(text);
 }
+
+// Language
+
+var languages = ["nl", "en"];
+var currLanguageIndex = 0;
+var currLanguage = languages[currLanguageIndex];
+
+window.onload = function init() {
+    changeLanguage();    
+}
+
+function cycleLanguage() {
+    currLanguageIndex = (currLanguageIndex + 1) % languages.length;
+    currLanguage = languages[currLanguageIndex];
+    changeLanguage(); 
+}
+
+function changeLanguage() {
+    languages.forEach(language => {
+        var displayStyle = "none";
+        if (language == currLanguage) {
+            displayStyle = "block";
+        }
+        var elements = document.getElementsByClassName(language)
+        for (var i = 0; i < elements.length; i++) {
+            elements.item(i).style.display = displayStyle;
+        }
+    });
+}
